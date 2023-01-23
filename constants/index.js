1c2a71c2b98be3b41d67a9da65b8983c9f106e2a
jitendra giriya <85019904+jitendragiriya@users.noreply.github.com>
2023-01-23 13:23:11 +0530

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,5 +1,5 @@
 exports.MONGO_URL =
-  "mongodb+srv://jitendra_giriya:passwordForAtTime@cluster0.jr137.mongodb.net/colorBracket?retryWrites=true&w=majority";
+  "";
 
 exports.JWT_EXPIRES_IN = 365;
 
